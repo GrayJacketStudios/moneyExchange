4a2d7f3bc457fadaf9658c869dad0ba8e50f1eac
Sebastian Cristi <s.cristicastillo@gmail.com>
2019-08-06 12:24:54 -0300

    se termina de crear el conversor

---
+++ index.js
@@ -0,0 +1,27 @@
+var express = require('express');
+const app = express();
+const axios = require('axios');
+const cheerio = require("cheerio");
+
+app.get('/:monedaOrigen/:valorInicial/:monedaDestino', function (req, res) {
+    getConvertion(req.params.valorInicial,req.params.monedaOrigen,req.params.monedaDestino)
+    .then(respuesta => {
+        res.send(respuesta);
+    })
+    .catch((err)=>{
+        res.send("Error de proceso, revisar datos.<br>"+err);
+    });
+});
+
+async function getConvertion(valorInicial, monedaOrigen, monedaDestino){
+    let val = await axios.get('https://www.google.com/search?q='+valorInicial+'+'+monedaOrigen+'+to+'+monedaDestino+'&oq='+valorInicial+'+'+monedaOrigen+'+to+'+monedaDestino)
+    
+    var $ = cheerio.load(val.data);
+    var res = $('div').next().children().children().children().children().children().children().children().children().children().text();
+    var token = res.split(" ");
+    return token[0];
+}
+
+app.listen(3000, function () {
+  console.log('Example app listening on port 3000!');
+});
